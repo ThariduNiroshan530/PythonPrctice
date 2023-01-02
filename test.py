4a1239cf83326aa66cb2a37ef5dd40f72c124aba
ThariduNiroshan530 <thariduniroshan232@gmail.com>
2023-01-01 23:58:27 -0800

    edited 05

--- test.py
+++ test.py
@@ -3,9 +3,48 @@ print(34+45)
 x=6
 y=7
 print(x==y)
+print("Tharidu")
+x=28
+y=76
+z=x+y
+print(z)
+print(type(1234566265366))
+a = 123
+print(type(a))
+st = {5,2,4,1}
+print(type(st))
 c = 2+3j
-type(c)
-y = 3 + 0j
-type(y)
 d = {1:'a', 2:'b', 3:'c'}
-type(d)
+print(d)
+print(type(d))
+a = "Tharidu"
+print(a.upper())
+a = "Hello"
+b = "world"
+c = a + b
+print(c)
+a = "Hello"
+b = "world"
+c = a + "" + b
+print(c)
+age = 36
+txt = "My name is john, and I am {}"
+print(txt.format(age))
+x = 6
+y = 5
+z = x+y
+print(z)
+x = 8.5
+print(type(x))
+y = 8+4j
+print(type(y))
+a = "Tharidu"
+print(a[1])
+for x in "banana":
+    print(x)
+p = "Tharidu"
+print(len(p))
+txt = "The best things in life are free"
+print("free" in txt)
+b = "Hello world"
+print(b[2:5])    
