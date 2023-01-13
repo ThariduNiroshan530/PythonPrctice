24fb91ad54cdff06e3a3885da4d88a36aea6fc03
Tharidu Niroshan <117273165+ThariduNiroshan530@users.noreply.github.com>
2023-01-13 10:53:34 -0800

    edited 13 (python set)

--- test.py
+++ test.py
@@ -273,3 +273,120 @@ thistuple = ("apple", "banana", "cherry")
 y = list(thistuple)
 y.remove("apple")
 thistuple = tuple(y)
+
+#python set
+
+myset = {"apple", "banana", "cherry"}
+print(myset)
+
+thisset = {"apple", "banana", "cherry"}
+print(thisset)
+
+thisset = {"apple", "banana", "cherry", "apple",}
+print(thisset)
+
+thisset = {"apple", "banana", "cherry"}
+print(len(thisset))
+
+set1 = {"apple", "banana", "cherry"}
+set2 ={1, 2, 3, 9, 7}
+set3 ={True, False, False}
+print(set1)
+print(set2)
+print(set3)
+
+set1 = {"abc", 34, True, 40, "male"}
+print(set1)
+
+myset = {"apple", "banana", "cherry"}
+print(type(myset))
+
+thisset = set(("apple", "banana", "cherry")) #note the double round brackets
+print(thisset)
+
+thisset = {"apple", "banana", "cherry"}
+for x in thisset:
+  print(x)
+
+thisset = {"apple", "banana", "cherry"}
+print("banana" in thisset)
+
+thisset = {"apple", "banana", "cherry"}
+thisset.add("orange")
+print(thisset)
+
+
+thisset = {"apple", "banana", "cherry"}
+tropical = {"pineapple", "mango", "papaya"}
+
+thisset.update(tropical)
+
+print(thisset)
+
+
+thisset = {"apple", "banana", "cherry"}
+mylist = ["kiwi", "orange"]
+
+thisset.update(mylist)
+
+print(thisset)
+
+
+thisset = {"apple", "banana", "cherry"}
+
+thisset.remove("banana")
+
+print(thisset)
+
+
+thisset = {"apple","banana", "cherry"}
+
+thisset.discard("banana")
+
+print(thisset)
+
+thisset = {"apple", "banana", "cherry"}
+
+x = thisset.pop()
+
+print(x) #removed item
+
+print(thisset)
+
+
+thisset = {"apple", "banana", "cherry"}
+
+thisset.clear()
+
+print(thisset)
+
+
+thisset = {"apple", "banana", "cherry"}
+
+for x in thisset:
+  print(x)
+
+
+set1 = {"a", "b", 'c'}
+set2 = {1, 2, 3}
+
+set3 = set1.union(set2)
+print(set3)
+
+
+set1 = {"a", "b", "c"}
+set2 = {1, 2, 3}
+
+set1.update(set2)
+print(set1)
+
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+
+x.intersection_update()
+
+print(x)
+
+
+
+
