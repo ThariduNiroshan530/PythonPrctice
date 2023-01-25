6795ad51df5943eebd7da74ace07cffed4660950
Tharidu Niroshan <117273165+ThariduNiroshan530@users.noreply.github.com>
2023-01-25 10:23:31 -0800

    edited 15

--- practice01.py
+++ practice01.py
@@ -164,4 +164,25 @@ price(txt.format(age, name))
 myorder = "I have a {carname}, it is a {model}."
 price(myorder.format(carname = "Ford", model = "Mustang"))
 
+print("Tharidu")
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+
+thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+print(thistuple)
+
+thistuple = ("apple", "banana", "cherry")
+print(len(thistuple))
+
+thistuple = ("apple",)
+print(type(thistuple))
+
+#NOT a tuple
+thistuple = ("apple")
+print(type(thistuple))
+
+tuple1 = ("apple", "banana", "cherry")
+tuple2 = (1, 5, 7, 9, 3)
+tuple3 = (True, False, False)
 
